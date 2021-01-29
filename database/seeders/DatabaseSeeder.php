eea93d595a9780793e96e7a23283fbe1b3ac886b
RodrigoAlanisWeb <rodri2007ro@gmail.com>
2021-01-28 21:22:08 -0600

    Sedders y Factorys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Curso;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        Curso::factory(50)->create();
     }
 }
